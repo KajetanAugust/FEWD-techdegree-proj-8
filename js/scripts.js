8e34ecb4d77e9c7553161b1da40dd6a8ff5aaabc
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-02-26 19:02:42 +0100

    Got data and created elements on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
 
 const cardsDiv = document.getElementById('employees');
 
+let users;
 
-
-
-fetch('https://fsjs-public-api-backup.herokuapp.com/api')
+fetch('https://randomuser.me/api/?results=12')
     .then(res => res.json())
     .then(data  => {
-        Object.values(data);
-        console.log(data);
+        users = (Object.values(data));
+        creatingUser(Object.values(data));
     });
 
 
+function creatingUser(data) {
+    data[0].map(person =>{
+        const userDiv = document.createElement('div');
+        cardsDiv.appendChild(userDiv);
+        userDiv.innerHTML = `
+        <img src=${person.picture.large} alt="profile picture">
+        <span>${person.name.first} ${person.name.last}</span>
+        <p>${person.email}</p>
+        <p>${person.location.city}</p>
+        `;
+        userDiv.className = 'user';
+    });
+}
