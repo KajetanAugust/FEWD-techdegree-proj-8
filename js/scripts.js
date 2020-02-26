1c2ce8404d51064a3b34b0efbcc26165f792c9f7
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-02-27 00:06:07 +0100

    small cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
-
 const cardsDiv = document.getElementById('employees');
 
 let users;
 
 fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(res => res.json())
-    .then(data  => {
+    .then(data => {
         users = (Object.values(data));
         creatingUser(Object.values(data));
     });
 
 
 function creatingUser(data) {
-    data[0].map(person =>{
+    data[0].map(person => {
         const userDiv = document.createElement('div');
         cardsDiv.appendChild(userDiv);
         userDiv.innerHTML = `
+
         <div class="photo">
         <img src=${person.picture.large} alt="profile picture">
         </div>
+        
         <div class="info">
         <h3 class="name">${person.name.first} ${person.name.last}</h3>
         <p class="email">${person.email}</p>
